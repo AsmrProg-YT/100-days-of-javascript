b5a3e4a95df4d049e61cbb1dc60c34d7c93e3df6
Reza Mehdikhanlou <mehdikhanloureza@gmail.com>
2023-02-28 22:54:15 +0330

    Adding Day #16

---
+++ Day #16 - Geometric Art Generator/index.js
@@ -0,0 +1,28 @@
+const btn = document.getElementById("btn");
+const shapes = [
+    "quad-circle-1",
+    "quad-circle-2",
+    "quad-circle-3",
+    "quad-circle-4",
+    "triangle-1",
+    "triangle-2",
+    "triangle-3",
+    "triangle-4",
+    "circle",
+];
+
+const colors = ["#01d2fd", "#ffc700", "#fe9f12", "#06d0c7"];
+const boxes = document.querySelectorAll(".container div");
+
+let generatePattern = () => {
+    boxes.forEach((box) => {
+        box.className = "";
+        let i = Math.floor(Math.random() * shapes.length);
+        let j = Math.floor(Math.random() * colors.length);
+        box.classList.add(shapes[i]);
+        box.style.backgroundColor = colors[j];
+    });
+};
+
+btn.addEventListener("click", generatePattern);
+window.addEventListener("load", generatePattern);
---
+++ Day #16 - Geometric Art Generator/style.css
@@ -0,0 +1,80 @@
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap');
+
+*{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+
+.wrapper{
+    position: absolute;
+    transform: translate(-50%, -50%);
+    left: 50%;
+    top: 50%;
+}
+
+.container{
+    background-color: #000;
+    height: calc(15vmin * 5);
+    width: calc(15vmin * 4);
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    padding: 15px;
+    gap: 10px;
+    border-radius: 10px;
+    box-shadow: 0 10px 30px rgba(90, 90, 100, 0.4);
+}
+
+.container div{
+    position: relative;
+    height: 100%;
+    width: 100%;
+}
+
+button{
+    width: 100%;
+    margin-top: 30px;
+    background-color: #000;
+    padding: 18px 0;
+    color: #fff;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    font-family: 'Poppins', sans-serif;
+    font-size: 3.5vmin;
+    border-radius: 5px;
+}
+
+.quad-circle-1{
+    clip-path: circle(100% at 0 0);
+}
+
+.quad-circle-2{
+    clip-path: circle(100% at 100% 0);
+}
+.quad-circle-3{
+    clip-path: circle(100% at 100% 100%);
+}
+.quad-circle-4{
+    clip-path: circle(100% at 0 100%);
+}
+
+.triangle-1{
+    clip-path: polygon(0 0, 0% 100%, 100% 0);
+}
+
+.triangle-2{
+    clip-path: polygon(0 0, 100% 0%, 100% 100%);
+}
+
+.triangle-3{
+    clip-path: polygon(0 100%, 100% 0%, 100% 100%);
+}
+
+.triangle-4{
+    clip-path: polygon(0 0, 0% 100%, 100% 100%);
+}
+
+.circle{
+    border-radius: 50%;
+}
