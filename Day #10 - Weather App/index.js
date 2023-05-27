b41b56ead4a0e819f28bf866e0983a137faf0306
Reza Mehdikhanlou <mehdikhanloureza@gmail.com>
2023-05-27 19:10:17 +0330

    Added Projects README.md

--- Day #10 - Weather App/index.js
+++ Day #10 - Weather App/index.js
@@ -6,7 +6,7 @@ const error404 = document.querySelector('.not-found');
 
 search.addEventListener('click', () => {
 
-    const APIKey = '728b0ee6df5687559812bd3169ad77b7';
+    const APIKey = 'Your Api Key';
     const city = document.querySelector('.search-box input').value;
 
     if (city === '')
