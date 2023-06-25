68350a0961ea272cdca7971f9444c758a2f85980
Reza Mehdikhanlou <mehdikhanloureza@gmail.com>
2023-06-26 03:10:08 +0330

    Adding Day #38

---
+++ Day #38 - Movie App/README.md
@@ -0,0 +1,17 @@
+# Day #38
+
+### Movie App (TheMovieDB)
+In this tutorial ([Open in Youtube](https://youtu.be/dButm3gpZDA)),  I am gonna showing to you how to code a Movie App using JavaScript. in this video i'm using TheMovieDB api to get movies info. Also this code is fully responsive and when you scroll it will load more results❗️
+
+## Warning
+You need to get your own api key (in video we showed how!) and replace it in index.js file on line 1 :
+
+```javascript
+const apiKey = "YOUR_API_KEY";
+```
+
+
+# Screenshot
+Here we have project screenshot :
+
+![screenshot](screenshot.jpg)
---
+++ Day #38 - Movie App/index.js
@@ -0,0 +1,120 @@
+const apiKey = "YOUR_API_KEY";
+const imgApi = "https://image.tmdb.org/t/p/w1280";
+const searchUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=`;
+const form = document.getElementById("search-form");
+const query = document.getElementById("search-input");
+const result = document.getElementById("result");
+
+let page = 1;
+let isSearching = false;
+
+// Fetch JSON data from url
+async function fetchData(url) {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error("Network response was not ok.");
+        }
+        return await response.json();
+    } catch (error) {
+        return null;
+    }
+}
+
+// Fetch and show results based on url
+async function fetchAndShowResult(url) {
+    const data = await fetchData(url);
+    if (data && data.results) {
+        showResults(data.results);
+    }
+}
+
+// Create movie card html template
+function createMovieCard(movie) {
+    const { poster_path, original_title, release_date, overview } = movie;
+    const imagePath = poster_path ? imgApi + poster_path : "./img-01.jpeg";
+    const truncatedTitle = original_title.length > 15 ? original_title.slice(0, 15) + "..." : original_title;
+    const formattedDate = release_date || "No release date";
+    const cardTemplate = `
+        <div class="column">
+            <div class="card">
+                <a class="card-media" href="./img-01.jpeg">
+                    <img src="${imagePath}" alt="${original_title}" width="100%" />
+                </a>
+                <div class="card-content">
+                    <div class="card-header">
+                        <div class="left-content">
+                        <h3 style="font-weight: 600">${truncatedTitle}</h3>
+                        <span style="color: #12efec">${formattedDate}</span>
+                        </div>
+                    <div class="right-content">
+                        <a href="${imagePath}" target="_blank" class="card-btn">See Cover</a>
+                    </div>
+                </div>
+                <div class="info">
+                    ${overview || "No overview yet..."}
+                </div>
+            </div>
+        </div>
+    </div>
+    `;
+    return cardTemplate;
+}
+
+// Clear result element for search
+function clearResults() {
+    result.innerHTML = "";
+}
+
+// Show results in page
+function showResults(item) {
+    const newContent = item.map(createMovieCard).join("");
+    result.innerHTML += newContent || "<p>No results found.</p>";
+}
+
+// Load more results
+async function loadMoreResults() {
+    if (isSearching) {
+        return;
+    }
+    page++;
+    const searchTerm = query.value;
+    const url = searchTerm ? `${searchUrl}${searchTerm}&page=${page}` : `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${apiKey}&page=${page}`;
+    await fetchAndShowResult(url);
+}
+
+// Detect end of page and load more results
+function detectEnd() {
+    const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
+    if (scrollTop + clientHeight >= scrollHeight - 20) {
+        loadMoreResults();
+    }
+}
+
+// Handle search
+async function handleSearch(e) {
+    e.preventDefault();
+    const searchTerm = query.value.trim();
+    if (searchTerm) {
+        isSearching = true;
+        clearResults();
+        const newUrl = `${searchUrl}${searchTerm}&page=${page}`;
+        await fetchAndShowResult(newUrl);
+        query.value = "";
+    }
+}
+
+// Event listeners
+form.addEventListener('submit', handleSearch);
+window.addEventListener('scroll', detectEnd);
+window.addEventListener('resize', detectEnd);
+
+// Initialize the page
+async function init() {
+    clearResults();
+    const url = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${apiKey}&page=${page}`;
+    isSearching = false;
+    await fetchAndShowResult(url);
+}
+
+init();
