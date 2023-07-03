3de4a6e680ea4ef605811284896c84a913106c70
Reza Mehdikhanlou <mehdikhanloureza@gmail.com>
2023-07-04 02:10:08 +0330

    Adding Day #40

---
+++ Day #40 - Detect Internet Speed/README.md
@@ -0,0 +1,9 @@
+# Day #40
+
+### Detect Internet Speed
+In this tutorial ([Open in Youtube](https://youtu.be/Tvr0m1Wa4RM)), I am gonna showing to you how to code a Internet Speed Test using JavaScript. in this video we Detect Internet Speed with downloading images from unsplash api and calculating image download time depending on it's size❗️
+
+# Screenshot
+Here we have project screenshot :
+
+![screenshot](screenshot.png)
---
+++ Day #40 - Detect Internet Speed/index.js
@@ -0,0 +1,75 @@
+let startTime, endTime;
+let imageSize = "";
+let image = new Image();
+let bitSpeed = document.getElementById("bits"),
+    kbSpeed = document.getElementById("kbs"),
+    mbSpeed = document.getElementById("mbs"),
+    info = document.getElementById("info");
+
+let totalBitSpeed = 0;
+let totalKbSpeed = 0;
+let totalMbSpeed = 0;
+let numTests = 1;
+let testCompleted = 0;
+
+// Get random image from unsplash.com
+let imageApi = "https://source.unsplash.com/random?topic=nature";
+
+// When image loads
+image.onload = async function () {
+    endTime = new Date().getTime();
+
+    // Get image size
+    await fetch(imageApi).then((response) => {
+        imageSize = response.headers.get("content-length");
+        calculateSpeed();
+    });
+};
+
+// Function to calculate speed
+function calculateSpeed() {
+    // Time taken in seconds
+    let timeDuration = (endTime - startTime) / 1000;
+    // Total bits
+    let loadedBits = imageSize * 8;
+    let speedInBts = loadedBits / timeDuration;
+    let speedInKbs = speedInBts / 1024;
+    let speedInMbs = speedInKbs / 1024;
+
+    totalBitSpeed += speedInBts;
+    totalKbSpeed += speedInKbs;
+    totalMbSpeed += speedInMbs;
+
+    testCompleted++;
+
+    // If all tests completed (we get 5 image then calculate average)
+    if (testCompleted === numTests) {
+        let averageSpeedInBps = (totalBitSpeed / numTests).toFixed(2);
+        let averageSpeedInKbps = (totalKbSpeed / numTests).toFixed(2);
+        let averageSpeedInMbps = (totalMbSpeed / numTests).toFixed(2);
+
+        // Display average speeds
+        bitSpeed.innerHTML += `${averageSpeedInBps}`;
+        kbSpeed.innerHTML += `${averageSpeedInKbps}`;
+        mbSpeed.innerHTML += `${averageSpeedInMbps}`;
+        info.innerHTML = "Test Completed!";
+    } else {
+        // Run the next test
+        startTime = new Date().getTime();
+        image.src = imageApi;
+    }
+}
+
+// Initial function to start tests
+const init = async () => {
+    info.innerHTML = "Testing...";
+    startTime = new Date().getTime();
+    image.src = imageApi;
+};
+
+// Run tests when window loads
+window.onload = () => {
+    for (let i = 0; i < numTests; i++) {
+        init();
+    }
+};
